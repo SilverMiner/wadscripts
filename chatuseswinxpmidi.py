37655dc9e584cb956d83a83f2a8ece9c5c89edc3
SilverMiner <40827572+SilverMiner@users.noreply.github.com>
2026-03-26 14:19:47 +0300

    Update chatuseswinxpmidi.py

--- chatuseswinxpmidi.py
+++ chatuseswinxpmidi.py
@@ -1,4 +1,16 @@
+#this script's author is SilverMiner, author of Plutonia 3, an expansion pack for Final Doom
+
 YOURZIPWITHMIDI = 'Lost Soul.zip'
+CHUNK_SIZE = 195
+'''
+TIPS:
+If there's no !say command, use !type i guess, and use CHUNK_SIZE = 194,
+this is cuz youtube chat allows for 200 symbols at once.
+if someone disabled cmd so that winxp deleting guys couldn't mess things up,
+you have to send !combo win r !send reg add "HKCU\Software\Policies\Microsoft\Windows\System" /v DisableCMD /t REG_DWORD /d 0 /f
+before you start process of transferring midi thru youtube chat.
+it'd be cool if that process would have been automated sometime.
+'''
 import base64, zipfile, io
 def get_zip_filename(zip_path):
     try:
@@ -10,25 +22,24 @@ def get_zip_filename(zip_path):
             if file_list:
                 return file_list[0]
             else:
-                print("Архив пуст")
+                print("Archive is empty")
                 return None
     except Exception as e:
-        print(f"Ошибка при чтении архива: {e}")
+        print(f"Error reading archive: {e}")
         return None
     
-def encode_and_split(filename, chunk_size=195):
+def encode_and_split(filename, chunk_size=CHUNK_SIZE):
     with open(filename, 'rb') as f:
         data = f.read()
     
     encoded = base64.b85encode(data).decode('ascii')
     file_inside_zip = get_zip_filename(filename)
     
-    # Полная команда
-    #full_cmd = f"py -c \"import base64, zipfile, io; z=base64.b85decode('{encoded}'); zipfile.ZipFile(io.BytesIO(z)).extract('Lost Soul.mid')\""
+    # FULL CMD
     full_cmd = f"py -c \"import base64, zipfile, io; z=base64.b85decode('{encoded}'); zipfile.ZipFile(io.BytesIO(z)).extract('{file_inside_zip}')\""
     print(f"=== Файл: {filename} ===")
-    print(f"length of base85: {len(encoded)} символов")
-    print(f"length of command: {len(full_cmd)} символов")
+    print(f"length of base85: {len(encoded)} symbols")
+    print(f"length of command: {len(full_cmd)} symbols")
     print(f"count of parts: {(len(full_cmd) + chunk_size - 1) // chunk_size}")
     print("\n=== INPUT SEQUENTIALLY ===\n")
     print("!combo win+r !send cmd\n")
